058b7a65d010734f2d0619ff406c74e72c3aca6b
= <arairikar1@gmail.com>
2025-06-11 17:16:30 -0500

    Created Lambda and SQS to trigger Lambda

---
+++ lambdas/get_train_status/get_train_status.py
@@ -0,0 +1,27 @@
+"""Module containing code for Lambda function to fetch CTA train statuses from the Train Tracker API."""
+from typing import Optional, Dict, Any
+import logging
+
+# Set up logger
+# TODO: convert the logging level into an environment variable
+logger = logging.getLogger('cta-train-analytics-get-train-status')
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
+    """Main handler function for the Lambda fetching recently played tracks."""
+    # Log basic information about the Lambda function
+    logger.info('Begin Lambda execution')
+    logger.info(f'Lambda request ID: {context.aws_request_id}')
+    logger.info(f'Lambda function name: {context.function_name}')
+    logger.info(f'Lambda function version: {context.function_version}')
+    logger.info(f'Event: {event}')
+
+    return {
+        'statusCode': 200,
+        'body': 'Execution successful'
+    }
--- main.tf
+++ main.tf
@@ -10,11 +10,11 @@ provider "aws" {
   }
 }
 
-module "dynamodb_table" {
+module "application_dynamodb_table" {
   source            = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/dynamodb?ref=main"
   environment       = var.environment
   project           = var.project_name
-  table_name        = "cta-train-tracker-location-data"
+  table_name        = "cta-train-tracker-location-application-data"
   hash_key          = "TrainId"
   range_key         = "UpdatedTimestamp"
   attributes        = [
@@ -28,4 +28,82 @@ module "dynamodb_table" {
     }
   ]
   enable_ttl        = true
+}
+
+module "sqs_queue" {
+  source                     = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/sqs-topic?ref=main"
+  queue_name                 = "cta-train-tracker-analytics-lambda-trigger-queue"
+  visibility_timeout_seconds = 60
+  project                    = var.project_name
+  environment                = var.environment
+}
+
+data "aws_iam_policy_document" "lambda_trust_relationship_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "lambda_get_cta_train_status_execution_role_inline_policy_document" {
+  statement {
+    effect    = "Allow"
+    actions = [
+      "sns:Publish"
+    ]
+    resources = [
+      var.sns_topic_arn
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+module "lambda_get_cta_train_status_execution_role" {
+  source                    = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/iam-role?ref=main"
+  role_name                 = "cta-lambda-get-cta-train-status-execution-role"
+  trust_relationship_policy = data.aws_iam_policy_document.lambda_trust_relationship_policy.json
+  inline_policy             = data.aws_iam_policy_document.lambda_get_cta_train_status_execution_role_inline_policy_document.json
+  inline_policy_description = "Inline policy for CTA train analytics train status Lambda function execution role"
+  environment               = var.environment
+  project                   = var.project_name
+}
+
+module "cta_get_train_status_lambda" {
+  source                         = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/lambda?ref=main"
+  environment                    = var.environment
+  project                        = var.project_name
+  lambda_name                    = "cta-get-train-status-lambda"
+  lambda_description             = "Lambda function to get current status of CTA trains from the CTA Train Tracker API"
+  lambda_filename                = "get_train_status.zip"
+  lambda_handler                 = "get_train_status.lambda_handler"
+  lambda_memory_size             = "256"
+  lambda_runtime                 = "python3.12"
+  lambda_timeout                 = 30
+  lambda_execution_role_arn      = module.lambda_get_recently_played_role.role_arn
+  sns_topic_arn                  = var.sns_topic_arn
+    lambda_environment_variables = {
+      API_KEY = var.cta_train_tracker_api_key
+  }
+}
+
+module "sqs_lambda_trigger" {
+  source              = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/sqs-lambda-trigger?ref=main"
+  queue_arn           = module.sqs_queue.queue_arn
+  lambda_function_arn = module.cta_get_train_status_lambda.lambda_arn
+  trigger_enabled     = true
+  batch_size          = 1
 }
