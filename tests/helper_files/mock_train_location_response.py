cea9219c4155f358df7ad4cf8b47a2a8b0f8b7d2
= <arairikar1@gmail.com>
2025-06-23 16:41:12 -0500

    Added unit tests for get_train_status lambda

---
+++ tests/helper_files/mock_train_location_response.py
@@ -0,0 +1,31 @@
+MOCK_TRAIN_LOCATION_RESPONSE = {
+  "ctatt": {
+    "tmst": "2025-06-20T12:43:12",
+    "errCd": "0",
+    "errNm": "null",
+    "route": [
+      {
+        "@name": "purple",
+        "train": [
+          {
+            "rn": "110",
+            "destSt": "30077",
+            "destNm": "Forest Park",
+            "trDr": "5",
+            "nextStaId": "40060",
+            "nextStpId": "30013",
+            "nextStaNm": "Belmont",
+            "prdt": "2025-06-20T12:42:56",
+            "arrT": "2025-06-20T12:43:56",
+            "isApp": "1",
+            "isDly": "0",
+            "flags": "null",
+            "lat": "41.94644",
+            "lon": "-87.71833",
+            "heading": "142"
+          }
+        ]
+      }
+    ]
+  }
+}
---
+++ tests/helper_files/mock_train_location_response_no_trains.py
@@ -0,0 +1,13 @@
+MOCK_TRAIN_LOCATION_NO_TRAINS_RESPONSE = {
+  "ctatt": {
+    "tmst": "2025-06-20T12:43:12",
+    "errCd": "0",
+    "errNm": "null",
+    "route": [
+      {
+        "@name": "purple",
+        "train": []
+      }
+    ]
+  }
+}
